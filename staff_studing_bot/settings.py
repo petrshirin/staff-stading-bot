ae0010a4ac6e5991883fec3e74d2923b9fdd8dcd
Petr <p.e.shirin@gmail.com>
2020-07-29 19:48:16 +0800

    fix db setting

--- staff_studing_bot/settings.py
+++ staff_studing_bot/settings.py
@@ -106,14 +106,20 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASS'],
-            'HOST': os.environ['DB_HOST'],
-            'PORT': '5432',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+    #DATABASES = {
+    #    'default': {
+    #        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #        'NAME': os.environ['DB_NAME'],
+    #        'USER': os.environ['DB_USER'],
+    #        'PASSWORD': os.environ['DB_PASS'],
+    #        'HOST': os.environ['DB_HOST'],
+    #        'PORT': '5432',
+    #    }
+    #}
 
 # Logining
 LOGGING = {
