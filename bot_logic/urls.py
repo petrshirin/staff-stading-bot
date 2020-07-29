8ea833e29567d2feee00285f59c3979bfa31ed00
Petr <p.e.shirin@gmail.com>
2020-07-29 20:23:50 +0800

    fix webhook urls

--- bot_logic/urls.py
+++ bot_logic/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from .views import get_web_hook
 
 urlpatterns = [
-    path('', get_web_hook, name="telegram webhook"),
+    path('<str:token>', get_web_hook, name="telegram webhook"),
 ]
